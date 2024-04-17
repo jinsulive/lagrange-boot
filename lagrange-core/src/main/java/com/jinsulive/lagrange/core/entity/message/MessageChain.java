dd11f2cd92ac1161d4eec8fd7fbbba8ad316524b
lxy <jinsulive@163.com>
2024-04-17 09:35:47 +0800

    feat: 支持发送图片和回复

--- lagrange-core/src/main/java/com/jinsulive/lagrange/core/entity/message/MessageChain.java
+++ lagrange-core/src/main/java/com/jinsulive/lagrange/core/entity/message/MessageChain.java
@@ -3,6 +3,8 @@ package com.jinsulive.lagrange.core.entity.message;
 import cn.hutool.json.JSONUtil;
 import com.jinsulive.lagrange.core.constant.message.MessageType;
 import com.jinsulive.lagrange.core.entity.message.data.At;
+import com.jinsulive.lagrange.core.entity.message.data.Image;
+import com.jinsulive.lagrange.core.entity.message.data.Reply;
 import com.jinsulive.lagrange.core.entity.message.data.Text;
 
 import java.util.LinkedList;
@@ -28,6 +30,29 @@ public class MessageChain {
         Message message = Message.builder().
                 type(MessageType.AT)
                 .data(At.builder().qq(qq).build())
+                .build();
+        messages.add(message);
+        return this;
+    }
+
+    public MessageChain image(Image image) {
+        Message message = Message.builder().
+                type(MessageType.IMAGE)
+                .data(image)
+                .build();
+        messages.add(message);
+        return this;
+    }
+
+    public MessageChain reply(String messageId) {
+        // 回复的引用消息只能有一条
+        boolean match = messages.stream().anyMatch(message -> message.getType() == MessageType.REPLY);
+        if (match) {
+            throw new IllegalArgumentException("Reply message can only have one message.");
+        }
+        Message message = Message.builder().
+                type(MessageType.REPLY)
+                .data(Reply.builder().id(String.valueOf(messageId)).build())
                 .build();
         messages.add(message);
         return this;
