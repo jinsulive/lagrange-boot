924ddd78691a9a81b928fcc13eb479524878b862
lxy <jinsulive@163.com>
2024-09-08 12:42:16 +0800

    fix: RequestEventMatcherService重复注入问题

--- lagrange-spring-boot-starter/src/main/java/com/jinsulive/lagrange/spring/autoconfigure/service/request/impl/RequestEventMatcherServiceImpl.java
+++ lagrange-spring-boot-starter/src/main/java/com/jinsulive/lagrange/spring/autoconfigure/service/request/impl/RequestEventMatcherServiceImpl.java
@@ -5,13 +5,11 @@ import com.jinsulive.lagrange.core.constant.request.RequestSubType;
 import com.jinsulive.lagrange.core.constant.request.RequestType;
 import com.jinsulive.lagrange.core.event.request.RequestEvent;
 import com.jinsulive.lagrange.spring.autoconfigure.service.request.RequestEventMatcherService;
-import org.springframework.stereotype.Service;
 
 /**
  * @author lxy
  * @since 2024年09月08日 03:47:24
  */
-@Service
 public class RequestEventMatcherServiceImpl implements RequestEventMatcherService {
 
     @Override
